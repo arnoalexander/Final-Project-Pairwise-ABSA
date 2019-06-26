09e8d42a18ad8a0d5817646618add974a795d802
Arno Alexander <arno2alexander@gmail.com>
2019-06-26 12:31:23 +0700

    Initialize preprocessor: tokenizer

---
+++ embedding/preprocessor.py
@@ -0,0 +1,29 @@
+from nltk.tokenize import RegexpTokenizer
+import re
+
+
+class Preprocessor:
+
+    @classmethod
+    def tokenize(cls, sentence):
+
+        """
+        Tokenize a sentence into list of token
+        """
+
+        if sentence is None:
+            return []
+        else:
+            result = sentence.lower()
+
+            result = re.sub(r'(\.+|\-+|\,+|\/+|\&+|\:+|\!+|\?+)', lambda x: ' ' + x.group()[0] + ' ', result)
+
+            tokenizer = RegexpTokenizer(r'[\w\.\-\,\/\&\:\!\?]+')
+            result = tokenizer.tokenize(result)
+
+            return result
+
+
+if __name__ == '__main__':
+    sent = input('Sentence: ')
+    print(Preprocessor.tokenize(sent))
