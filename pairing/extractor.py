a1d4abb9d984281473031bedbe43ec6bb9b321cd
Arno Alexander <arno2alexander@gmail.com>
2019-07-27 00:47:51 +0700

    Add filter for embedding vector

--- pairing/extractor.py
+++ pairing/extractor.py
@@ -114,15 +114,32 @@ class Extractor:
     def _extract_features_aspect_sentiment(self, aspect, sentiment, tokens):
         result = dict()
 
-        # Intermediate Variables
+        # Intermediate Values
         dictionary_tf = self._extract_feature_dict_tf(tokens)
-        aspect_embedding = self._extract_feature_vector_sequence(aspect['start'], aspect['length'], tokens)
-        sentiment_embedding = self._extract_feature_vector_sequence(sentiment['start'], sentiment['length'], tokens)
-        sentence_embedding = self._extract_feature_vector_sentence(tokens)
+        if self.embedding_model is not None:
+            aspect_embeddings = [self._extract_featre_vector_word(token)
+                                 for token in tokens[aspect['start']:aspect['start']+aspect['length']]]
+            sentiment_embeddings = [self._extract_featre_vector_word(token)
+                                    for token in tokens[sentiment['start']:sentiment['start'] + sentiment['length']]]
+            sentence_embedding = self._extract_feature_vector_sentence(tokens)
 
-        # TODO remove this upon real implementation. only for test purpose
-        # result['aspect'] = tokens[aspect['start']]
-        # result['sentiment'] = tokens[sentiment['start']]
+            if not np.any(aspect_embeddings) or not np.any(sentiment_embeddings):
+                min_distance_aspect_sentiment = np.nan
+                aspect_chosen_embedding = self._extract_feature_vector_sequence(aspect['start'], aspect['length'], tokens)
+                sentiment_chosen_embedding = self._extract_feature_vector_sequence(sentiment['start'], sentiment['length'], tokens)
+            else:
+                aspect_embeddings = [aspect_embedding for aspect_embedding in aspect_embeddings if np.any(aspect_embedding)]
+                sentiment_embeddings = [sentiment_embedding for sentiment_embedding in sentiment_embeddings if np.any(sentiment_embedding)]
+                min_distance_aspect_sentiment = np.inf
+                aspect_chosen_embedding = None
+                sentiment_chosen_embedding = None
+                for aspect_embedding in aspect_embeddings:
+                    for sentiment_embedding in sentiment_embeddings:
+                        distance_aspect_sentiment = self._extract_feature_cosine_distance(aspect_embedding, sentiment_embedding)
+                        if distance_aspect_sentiment < min_distance_aspect_sentiment:
+                            min_distance_aspect_sentiment = distance_aspect_sentiment
+                            aspect_chosen_embedding = aspect_embedding
+                            sentiment_chosen_embedding = sentiment_embedding
 
         # Statistics Feature
         if self.word_count_model is not None:
@@ -140,18 +157,24 @@ class Extractor:
         result['dist_endpoint'] = self._extract_feature_dist_endpoint(aspect, sentiment)
 
         # Semantic Feature
-        for i in range(len(aspect_embedding)):
-            result['v_aspect_{}'.format(i)] = aspect_embedding[i]
-        for i in range(len(sentiment_embedding)):
-            result['v_sentiment_{}'.format(i)] = sentiment_embedding[i]
-        for i in range(len(sentence_embedding)):
-            result['v_sentence_{}'.format(i)] = sentence_embedding[i]
+        if self.embedding_model is not None:
+            for i in range(len(aspect_chosen_embedding)):
+                result['v_aspect_{}'.format(i)] = aspect_chosen_embedding[i]
+            for i in range(len(sentiment_chosen_embedding)):
+                result['v_sentiment_{}'.format(i)] = sentiment_chosen_embedding[i]
+            for i in range(len(sentence_embedding)):
+                result['v_sentence_{}'.format(i)] = sentence_embedding[i]
 
         # Similarity Feature
         if self.embedding_model is not None:
-            result['cos_aspect_sentiment'] = self._extract_feature_cosine_distance(aspect_embedding, sentiment_embedding)
-            result['cos_aspect_sentence'] = self._extract_feature_cosine_distance(aspect_embedding, sentence_embedding)
-            result['cos_sentiment_sentence'] = self._extract_feature_cosine_distance(sentiment_embedding, sentence_embedding)
+            if np.isnan(min_distance_aspect_sentiment):
+                result['cos_aspect_sentiment_validity'] = 0
+                result['cos_aspect_sentiment'] = -1
+            else:
+                result['cos_aspect_sentiment_validity'] = 1
+                result['cos_aspect_sentiment'] = min_distance_aspect_sentiment
+            result['cos_aspect_sentence'] = self._extract_feature_cosine_distance(aspect_chosen_embedding, sentence_embedding)
+            result['cos_sentiment_sentence'] = self._extract_feature_cosine_distance(sentiment_chosen_embedding, sentence_embedding)
 
         return result
 
@@ -166,14 +189,13 @@ class Extractor:
         return aspect['start'] - (sentiment['start'] + sentiment['length'] - 1)
 
     def _extract_feature_vector_sequence(self, start, length, tokens):
-        if self.embedding_model is None:
-            return np.array([])
         return self.embedding_model.get_vector_sentence(sentence=tokens[start:start+length], min_n=length, max_n=length)
 
     def _extract_feature_vector_sentence(self, tokens):
-        if self.embedding_model is None:
-            return np.array([])
         return self.embedding_model.get_vector_sentence(sentence=tokens)
+
+    def _extract_featre_vector_word(self, token):
+        return self.embedding_model.get_vector_word(word=token)
 
     @staticmethod
     def _extract_feature_dict_tf(tokens):
@@ -207,3 +229,4 @@ if __name__ == '__main__':
     extracted = extractor.extract_data(data)
     print(extracted.values.shape)
     print(extracted)
+    print(extracted.columns.values)
--- utility/embedding.py
+++ utility/embedding.py
@@ -87,8 +87,8 @@ class Embedding:
         result = np.zeros(self.model.wv.vector_size, np.float32)
         for ngram in ngrams:
             try:
-                ngram_vector = self._get_vector_ngram_word(
-                    ngram_word=ngram, use_norm_word=use_norm_word, use_norm_ngram_char=use_norm_ngram_char,
+                ngram_vector = self._get_vector_ngram_words(
+                    ngram_words=ngram, use_norm_word=use_norm_word, use_norm_ngram_char=use_norm_ngram_char,
                     handle_oov=False)
                 if use_norm_ngram_word:
                     ngram_vector = self.normalize_vector(ngram_vector)
@@ -123,10 +123,10 @@ class Embedding:
             result.append(sentence[idx_start:idx_start + n])
         return result
 
-    def _get_vector_ngram_word(self, ngram_word, use_norm_word=False, use_norm_ngram_char=False, handle_oov=True):
+    def _get_vector_ngram_words(self, ngram_words, use_norm_word=False, use_norm_ngram_char=False, handle_oov=True):
         words_found = 0
         result = np.zeros(self.model.wv.vector_size, np.float32)
-        for word in ngram_word:
+        for word in ngram_words:
             try:
                 word_vector = self.get_vector_word(word=word, use_norm=use_norm_ngram_char, handle_oov=False)
                 if use_norm_word:
